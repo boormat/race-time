88854f4ca21c69154713e1a6a6f4806d027d087f
Mathew Boorman <mathew.boorman@gmail.com>
2013-05-19 21:33:29 +1000

    Remove form tags.
    Removed forms tags, which do not help a metoer app, as you just have to override keystrokes.

--- client/race_submit.js
+++ client/race_submit.js
@@ -1,43 +1,53 @@
+// Note do not use forms.  It makes more work as you just have to override more stuff (i.e. keystrokes!)
+
 Template.raceSubmit.events({
-	'click h2': function () {
-		Meteor._debug('unclick');
-		Session.set('race_id', null);
-	},
 	'click .save':onSave,
 	'click .cancel':onCancel,
 });
 
-Template.raceSubmit.meh = "YOYO";
-
 function onCancel(event) {
-	event.preventDefault();
 	Meteor._debug('loggy cancel', this, event);
 	goTo("pickRace");
 }
 
-function onSave(event) {
-	event.preventDefault();
+function onSave(event, template) {
+	loggy(event, template);
 
-	var post = {
-			url: $(event.target).find('[name=url]').val(),
-			title: $(event.target).find('[name=title]').val(),
-			message: $(event.target).find('[name=message]').val()
-	};
+	var name = $(event.target).find('[name=name]').val();
+//	title: $(event.target).find('[name=title]').val(),
+//	message: $(event.target).find('[name=message]').val()
+	
+	if (! name )
+	{
+		throwError("Enter a race name");
+		return;
+	}
 
-	Meteor.call('post', post, function(error, id) {
-		if (error) {
-			// display the error to the user
-			throwError(error.reason);
+	var race_id = Races.insert({name: name});
+	Session.set('race_id', race_id);
+	goTo('editRace');
+};
 
-			// if the error is that the post already exists, take us there
-			if (error.error === 302)
-				Meteor.Router.to('postPage', error.details);
+//	var post = {
+//			url: $(event.target).find('[name=url]').val(),
+//			title: $(event.target).find('[name=title]').val(),
+//			message: $(event.target).find('[name=message]').val()
+//	};
+//
+//	Meteor.call('post', post, function(error, id) {
+//		if (error) {
+//			// display the error to the user
+//			throwError(error.reason);
+//
+//			// if the error is that the post already exists, take us there
+//			if (error.error === 302)
+//				Meteor.Router.to('postPage', error.details);
+//
+//		} 
+//		else {
+//			Meteor.Router.to('postPage', id);
+//		}
+//	});
 
-		} 
-		else {
-			Meteor.Router.to('postPage', id);
-		}
-	});
-}
 
 
